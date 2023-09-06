8db16b36a9c50ed7ab932f93442d54b4b2c7947d
hugo <hugof16silva@gmail.com>
2023-09-06 09:06:28 -0300

    corrigindo a formula

--- area-cilindro.js
+++ area-cilindro.js
@@ -3,5 +3,5 @@ const altura = 1
 
 console.log(`
 Para um cilindro com raio de 3 e altura de 1.
-A área total do cilindro: ${(2 * Math.PI * (raio + altura)).toFixed(0)}pi
+A área total do cilindro: ${(2 * Math.PI * raio * (raio + altura)).toFixed(0)}pi
 `);
